ea5c3ea0a4de3053bfaa7c8881fb538f09eeef96
naromori <a@dolta.su>
2025-10-05 13:38:31 +0300

    Fix: added missing import, changed to pathbuf

--- src/repl.rs
+++ src/repl.rs
@@ -29,6 +29,8 @@
 //! }
 //! ```
 
+use std::path::PathBuf;
+
 use rustyline::{error::ReadlineError, DefaultEditor, Result};
 
 
@@ -166,7 +168,7 @@ impl <H: CommandHandler> Repl<H> {
     /// let mut repl = Repl::new("> ", MyHandler).unwrap();
     /// let _ = repl.load_history(".my_app_history");
     /// ```
-    pub fn load_history(&mut self, path: &Path) -> Result<()> {
+    pub fn load_history(&mut self, path: &PathBuf) -> Result<()> {
         self.editor.load_history(path)
     }
 
@@ -195,7 +197,7 @@ impl <H: CommandHandler> Repl<H> {
     /// // ... run the REPL ...
     /// let _ = repl.save_history(".my_app_history");
     /// ```
-    pub fn save_history(&mut self, path: &Path) -> Result<()> {
+    pub fn save_history(&mut self, path: &PathBuf) -> Result<()> {
         self.editor.save_history(path)
     }
 
